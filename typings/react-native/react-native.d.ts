a00de904b480f75c74c84ee9c2871553045ea387
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2016-09-28 15:30:56 +0300

    Update Modal component

--- typings/react-native/react-native.d.ts
+++ typings/react-native/react-native.d.ts
@@ -3177,16 +3177,47 @@ declare namespace  __React {
         // Only `animated` is documented. The JS code says `animated` is
         // deprecated and `animationType` is preferred.
         animated?: boolean
+        /**
+         * The `animationType` prop controls how the modal animates.
+         *
+         * - `slide` slides in from the bottom
+         * - `fade` fades into view
+         * - `none` appears without an animation
+         */
         animationType?: "none" | "slide" | "fade"
+        /**
+         * The `transparent` prop determines whether your modal will fill the entire view.
+         * Setting this to `true` will render the modal over a transparent background.
+         */
         transparent?: boolean
+        /**
+         * The `visible` prop determines whether your modal is visible.
+         */
         visible?: boolean
+        /**
+         * The `onRequestClose` prop allows passing a function that will be called once the modal has been dismissed.
+         * _On the Android platform, this is a required function._
+         */
         onRequestClose?: () => void
+        /**
+         * The `onShow` prop allows passing a function that will be called once the modal has been shown.
+         */
         onShow?: (event: NativeSyntheticEvent<any>) => void
-
+        /**
+         * The `supportedOrientations` prop allows the modal to be rotated to any of the specified orientations.
+         * On iOS, the modal is still restricted by what's specified in your app's Info.plist's UISupportedInterfaceOrientations field.
+         * @platform ios
+         */
+        supportedOrientations: ('portrait' | 'portrait-upside-down' | 'landscape' | 'landscape-left' | 'landscape-right')[]
+        /**
+         * The `onOrientationChange` callback is called when the orientation changes while the modal is being displayed.
+         * The orientation provided is only 'portrait' or 'landscape'. This callback is also called on initial render, regardless of the current orientation.
+         * @platform ios
+         */
+        onOrientationChange: () => void,
     }
 
     export interface ModalStatic extends React.ComponentClass<ModalProperties> {
-
     }
 
     export interface TouchableWithoutFeedbackAndroidProperties {
