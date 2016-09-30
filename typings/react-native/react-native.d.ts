3f232c4448d9e7fb3243b88c0b11a77eabd3fee3
daserge <v-seshak@microsoft.com>
2016-09-30 17:02:57 +0300

    [APIs] Alert (+AlertAndroid)

--- typings/react-native/react-native.d.ts
+++ typings/react-native/react-native.d.ts
@@ -5051,10 +5051,59 @@ declare namespace  __React {
       style?: "default" | "cancel" | "destructive"
     }
 
-    export interface AlertStatic {
-       alert: (title: string, message?: string, buttons?: AlertButton[], type?: string) => void
+    interface AlertOptions {
+        /** @platform android */
+        cancelable?: boolean;
     }
 
+    /**
+     * Launches an alert dialog with the specified title and message.
+     *
+     * Optionally provide a list of buttons. Tapping any button will fire the
+     * respective onPress callback and dismiss the alert. By default, the only
+     * button will be an 'OK' button.
+     *
+     * This is an API that works both on iOS and Android and can show static
+     * alerts. To show an alert that prompts the user to enter some information,
+     * see `AlertIOS`; entering text in an alert is common on iOS only.
+     *
+     * ## iOS
+     *
+     * On iOS you can specify any number of buttons. Each button can optionally
+     * specify a style, which is one of 'default', 'cancel' or 'destructive'.
+     *
+     * ## Android
+     *
+     * On Android at most three buttons can be specified. Android has a concept
+     * of a neutral, negative and a positive button:
+     *
+     *   - If you specify one button, it will be the 'positive' one (such as 'OK')
+     *   - Two buttons mean 'negative', 'positive' (such as 'Cancel', 'OK')
+     *   - Three buttons mean 'neutral', 'negative', 'positive' (such as 'Later', 'Cancel', 'OK')
+     *
+     * ```
+     * // Works on both iOS and Android
+     * Alert.alert(
+     *   'Alert Title',
+     *   'My Alert Msg',
+     *   [
+     *     {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+     *     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+     *     {text: 'OK', onPress: () => console.log('OK Pressed')},
+     *   ]
+     * )
+     * ```
+     */
+    export interface AlertStatic {
+        alert: (title: string, message?: string, buttons?: AlertButton[], options?: AlertOptions, type?: string) => void
+    }
+
+    /**
+     * Wrapper around the Android native module.
+     */
+    export interface AlertAndroidStatic {
+        alert: (title: string, message?: string, buttons?: AlertButton[], options?: AlertOptions) => void
+    }
 
     /**
      * //FIXME: No documentation - inferred from RCTAdSupport.m
@@ -6642,6 +6691,9 @@ declare namespace  __React {
 
     export var Alert: AlertStatic
     export type Alert = AlertStatic
+
+    export var AlertAndroid: AlertAndroidStatic
+    export type AlertAndroid = AlertAndroidStatic
 
     export var AlertIOS: AlertIOSStatic
     export type AlertIOS = AlertIOSStatic
