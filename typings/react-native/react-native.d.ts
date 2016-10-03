48167e0f59f5adce65011e7aeb905943f9e04e21
daserge <v-seshak@microsoft.com>
2016-10-03 16:42:32 +0300

    [APIs] ImageStore

--- typings/react-native/react-native.d.ts
+++ typings/react-native/react-native.d.ts
@@ -7503,6 +7503,55 @@ declare namespace  __React {
         openSelectDialog(config: OpenSelectDialogOptions, successCallback: (args:ImagePickerResult) => void, cancelCallback: (args:any[]) => void): void
     }
 
+    export interface ImageStoreStatic {
+          /**
+           * Check if the ImageStore contains image data for the specified URI.
+           * @platform ios
+           */
+          hasImageForTag(uri: string, callback: (hasImage: boolean) => void): void
+          /**
+           * Delete an image from the ImageStore. Images are stored in memory and
+           * must be manually removed when you are finished with them, otherwise they
+           * will continue to use up RAM until the app is terminated. It is safe to
+           * call `removeImageForTag()` without first calling `hasImageForTag()`, it
+           * will simply fail silently.
+           * @platform ios
+           */
+          removeImageForTag(uri: string): void
+          /**
+           * Stores a base64-encoded image in the ImageStore, and returns a URI that
+           * can be used to access or display the image later. Images are stored in
+           * memory only, and must be manually deleted when you are finished with
+           * them by calling `removeImageForTag()`.
+           *
+           * Note that it is very inefficient to transfer large quantities of binary
+           * data between JS and native code, so you should avoid calling this more
+           * than necessary.
+           * @platform ios
+           */
+          addImageFromBase64(
+              base64ImageData: string,
+              success: (uri: string) => void,
+              failure: (error: any) => void
+          ): void
+          /**
+           * Retrieves the base64-encoded data for an image in the ImageStore. If the
+           * specified URI does not match an image in the store, the failure callback
+           * will be called.
+           *
+           * Note that it is very inefficient to transfer large quantities of binary
+           * data between JS and native code, so you should avoid calling this more
+           * than necessary. To display an image in the ImageStore, you can just pass
+           * the URI to an `<Image/>` component; there is no need to retrieve the
+           * base64 data.
+           */
+          getBase64ForTag(
+              uri: string,
+              success: (base64ImageData: string) => void,
+              failure: (error: any) => void
+          ): void
+    }
+
     // Network Polyfill
     // TODO: Add proper support for fetch
     export type fetch = (url: string, options?: Object) => Promise<any>
@@ -7664,12 +7713,15 @@ declare namespace  __React {
     export var Image: ImageStatic
     export type Image = ImageStatic
 
-    // TODO: ImageStore, KeyboardAvoidingView
+    // TODO: KeyboardAvoidingView
     export var ImageEditor: ImageEditorStatic
     export type ImageEditor = ImageEditorStatic
 
     export var ImagePickerIOS: ImagePickerIOSStatic
     export type ImagePickerIOS = ImagePickerIOSStatic
+
+    export var ImageStore: ImageStoreStatic
+    export type ImageStore = ImageStoreStatic
 
     export var LayoutAnimation: LayoutAnimationStatic
     export type LayoutAnimation = LayoutAnimationStatic
