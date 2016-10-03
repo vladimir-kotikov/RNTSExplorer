fa2f0e52e68dc007dff17686ae3ab7e188a00c5b
daserge <v-seshak@microsoft.com>
2016-10-03 18:05:57 +0300

    [APIs] InteractionManager, EventEmitter

--- typings/react-native/react-native.d.ts
+++ typings/react-native/react-native.d.ts
@@ -5071,20 +5071,230 @@ declare namespace  __React {
         set( dims: {[key: string]: any} ): void
     }
 
+    export type SimpleTask = {
+        name: string
+        gen: () => void
+    }
     export type PromiseTask = {
+        name: string
         gen: () => Promise<any>
     }
 
     export type Handle = number
 
+    /**
+     * EventSubscription represents a subscription to a particular event. It can
+     * remove its own subscription.
+     */
+    export class EventSubscription {
+        eventType: string;
+        key: number;
+        subscriber: EventSubscriptionVendor;
+
+        /**
+         * @param {EventSubscriptionVendor} subscriber the subscriber that controls
+         *   this subscription.
+         */
+        constructor(subscriber: EventSubscriptionVendor)
+        /**
+         * Removes this subscription from the subscriber that controls it.
+         */
+        remove(): void
+    }
+
+    /**
+     * EventSubscriptionVendor stores a set of EventSubscriptions that are
+     * subscribed to a particular event type.
+     */
+    export class EventSubscriptionVendor {
+        constructor()
+        /**
+         * Adds a subscription keyed by an event type.
+         *
+         * @param {string} eventType
+         * @param {EventSubscription} subscription
+         */
+        addSubscription(
+            eventType: string, subscription: EventSubscription): EventSubscription
+        /**
+         * Removes a bulk set of the subscriptions.
+         *
+         * @param {?string} eventType - Optional name of the event type whose
+         *   registered supscriptions to remove, if null remove all subscriptions.
+         */
+        removeAllSubscriptions(eventType?: string): void
+        /**
+         * Removes a specific subscription. Instead of calling this function, call
+         * `subscription.remove()` directly.
+         *
+         * @param {object} subscription
+         */
+        removeSubscription(subscription: Object): void
+        /**
+         * Returns the array of subscriptions that are currently registered for the
+         * given event type.
+         *
+         * Note: This array can be potentially sparse as subscriptions are deleted
+         * from it when they are removed.
+         *
+         * TODO: This returns a nullable array. wat?
+         *
+         * @param {string} eventType
+         * @returns {?array}
+         */
+        getSubscriptionsForType(eventType: string): EventSubscription[] 
+    }
+
+    /**
+     * @class EventEmitter
+     * @description
+     * An EventEmitter is responsible for managing a set of listeners and publishing
+     * events to them when it is told that such events happened. In addition to the
+     * data for the given event it also sends a event control object which allows
+     * the listeners/handlers to prevent the default behavior of the given event.
+     *
+     * The emitter is designed to be generic enough to support all the different
+     * contexts in which one might want to emit events. It is a simple multicast
+     * mechanism on top of which extra functionality can be composed. For example, a
+     * more advanced emitter may use an EventHolder and EventFactory.
+     */
+    export class EventEmitterStatic {
+        /**
+         * @constructor
+         *
+         * @param {EventSubscriptionVendor} subscriber - Optional subscriber instance
+         *   to use. If omitted, a new subscriber will be created for the emitter.
+         */
+        constructor(subscriber?: EventSubscriptionVendor)
+        /**
+         * Adds a listener to be invoked when events of the specified type are
+         * emitted. An optional calling context may be provided. The data arguments
+         * emitted will be passed to the listener function.
+         *
+         * TODO: Annotate the listener arg's type. This is tricky because listeners
+         *       can be invoked with varargs.
+         *
+         * @param {string} eventType - Name of the event to listen to
+         * @param {function} listener - Function to invoke when the specified event is
+         *   emitted
+         * @param {*} context - Optional context object to use when invoking the
+         *   listener
+         */
+        addListener(
+            eventType: string, listener: Function, context?: Object): EmitterSubscription
+        /**
+         * Similar to addListener, except that the listener is removed after it is
+         * invoked once.
+         *
+         * @param {string} eventType - Name of the event to listen to
+         * @param {function} listener - Function to invoke only once when the
+         *   specified event is emitted
+         * @param {*} context - Optional context object to use when invoking the
+         *   listener
+         */
+        once(
+            eventType: string, listener: Function, context?: Object): EmitterSubscription
+        /**
+         * Removes all of the registered listeners, including those registered as
+         * listener maps.
+         *
+         * @param {?string} eventType - Optional name of the event whose registered
+         *   listeners to remove
+         */
+        removeAllListeners(eventType?: string): void
+        /**
+         * Provides an API that can be called during an eventing cycle to remove the
+         * last listener that was invoked. This allows a developer to provide an event
+         * object that can remove the listener (or listener map) during the
+         * invocation.
+         *
+         * If it is called when not inside of an emitting cycle it will throw.
+         *
+         * @throws {Error} When called not during an eventing cycle
+         *
+         * @example
+         *   var subscription = emitter.addListenerMap({
+         *     someEvent: function(data, event) {
+         *       console.log(data);
+         *       emitter.removeCurrentListener();
+         *     }
+         *   });
+         *
+         *   emitter.emit('someEvent', 'abc'); // logs 'abc'
+         *   emitter.emit('someEvent', 'def'); // does not log anything
+         */
+        removeCurrentListener(): void
+        /**
+         * Removes a specific subscription. Called by the `remove()` method of the
+         * subscription itself to ensure any necessary cleanup is performed.
+         */
+        removeSubscription(subscription: EmitterSubscription): void
+         /**
+         * Returns an array of listeners that are currently registered for the given
+         * event.
+         *
+         * @param {string} eventType - Name of the event to query
+         * @returns {array}
+         */
+        listeners(eventType: string): EmitterSubscription[]
+         /**
+         * Emits an event of the given type with the given data. All handlers of that
+         * particular type will be notified.
+         *
+         * @param {string} eventType - Name of the event to emit
+         * @param {...*} Arbitrary arguments to be passed to each registered listener
+         *
+         * @example
+         *   emitter.addListener('someEvent', function(message) {
+         *     console.log(message);
+         *   });
+         *
+         *   emitter.emit('someEvent', 'abc'); // logs 'abc'
+         */
+        emit(eventType: string): void
+        /**
+         * Removes the given listener for event of specific type.
+         *
+         * @param {string} eventType - Name of the event to emit
+         * @param {function} listener - Function to invoke when the specified event is
+         *   emitted
+         *
+         * @example
+         *   emitter.removeListener('someEvent', function(message) {
+         *     console.log(message);
+         *   }); // removes the listener if already registered
+         *
+         */
+        removeListener(eventType: String, listener: Function): void
+    }
+
+    /**
+     * EmitterSubscription represents a subscription with listener and context data.
+     */
+    export class EmitterSubscription {
+        constructor(
+            emitter: EventEmitter,
+            subscriber: EventSubscriptionVendor,
+            listener: Function,
+            context?: Object
+        )
+    }
+
     export interface InteractionManagerStatic {
+        Events: {
+            interactionStart: string
+            interactionComplete: string
+        }
+
+        addListener(
+            eventType: string, listener: Function, context?: Object): EmitterSubscription
 
         /**
          * Schedule a function to run after all interactions have completed.
          * Returns a cancellable
          * @param fn
          */
-        runAfterInteractions( fn: () => void | PromiseTask):
+        runAfterInteractions( task: Function | SimpleTask | PromiseTask):
           {then: Function, done: Function, cancel: Function}
 
         /**
@@ -7948,6 +8158,9 @@ declare namespace  __React {
     export var DeviceEventSubscription: DeviceEventSubscriptionStatic
     export type DeviceEventSubscription = DeviceEventSubscriptionStatic
     export var InteractionManager: InteractionManagerStatic
+
+    export var EventEmitter: EventEmitterStatic
+    export type EventEmitter = EventEmitterStatic
 
     /** http://facebook.github.io/react-native/blog/2016/08/19/right-to-left-support-for-react-native-apps.html */
     export var I18nManager: I18nManagerStatic
