b179f811e29b16c2133b56e79e395a9978038d64
daserge <v-seshak@microsoft.com>
2016-10-04 18:12:36 +0300

    [APIs] TimePickerAndroid

--- typings/react-native/react-native.d.ts
+++ typings/react-native/react-native.d.ts
@@ -7293,21 +7293,42 @@ declare namespace  __React {
         is24Hour?: boolean
     }
 
+    /**
+     * Opens the standard Android time picker dialog.
+     *
+     * ### Example
+     *
+     * ```
+     * try {
+     *   const {action, hour, minute} = await TimePickerAndroid.open({
+     *     hour: 14,
+     *     minute: 0,
+     *     is24Hour: false, // Will display '2 PM'
+     *   });
+     *   if (action !== TimePickerAndroid.dismissedAction) {
+     *     // Selected hour (0-23), minute (0-59)
+     *   }
+     * } catch ({code, message}) {
+     *   console.warn('Cannot open time picker', message);
+     * }
+     * ```
+     */
     export interface TimePickerAndroidStatic {
 
         /**
-         * The available keys for the options object are:
-         * hour (0-23) - the hour to show, defaults to the current time
-         * minute (0-59) - the minute to show, defaults to the current time
-         * is24Hour (boolean) - If true, the picker uses the 24-hour format.
-         * If false, the picker shows an AM/PM chooser. If undefined,
-         * the default for the current locale is used.
-
-         * Returns a Promise which will be invoked an object containing action,
-         * hour (0-23), minute (0-59) if the user picked a time. If the user
-         * dismissed the dialog, the Promise will still be resolved with action
-         * being TimePickerAndroid.dismissedAction and all the other keys being
-         * undefined. Always check whether the action before reading the values.
+         * Opens the standard Android time picker dialog.
+         *
+         * The available keys for the `options` object are:
+         *   * `hour` (0-23) - the hour to show, defaults to the current time
+         *   * `minute` (0-59) - the minute to show, defaults to the current time
+         *   * `is24Hour` (boolean) - If `true`, the picker uses the 24-hour format. If `false`,
+         *     the picker shows an AM/PM chooser. If undefined, the default for the current locale
+         *     is used.
+         *
+         * Returns a Promise which will be invoked an object containing `action`, `hour` (0-23),
+         * `minute` (0-59) if the user picked a time. If the user dismissed the dialog, the Promise will
+         * still be resolved with action being `TimePickerAndroid.dismissedAction` and all the other keys
+         * being undefined. **Always** check whether the `action` before reading the values.
          */
         open(options: TimePickerAndroidOpenOptions): Promise<{action: string, hour: number, minute: number}>
 
